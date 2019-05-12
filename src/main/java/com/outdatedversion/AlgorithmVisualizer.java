c797c5192ef0c76ed6aa3e4fe75de536600aafca
Ben Watkins <ben@outdatedversion.com>
2019-05-11 19:03:39 -0500

    base project

---
+++ src/main/java/com/outdatedversion/AlgorithmVisualizer.java
@@ -0,0 +1,21 @@
+package com.outdatedversion;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Ben Watkins
+ * @since May/11/2019
+ */
+public class AlgorithmVisualizer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
